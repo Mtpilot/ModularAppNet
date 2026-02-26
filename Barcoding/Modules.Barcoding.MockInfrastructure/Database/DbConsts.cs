3866cfb5ecda391ec08297a580b2e52870b03653
GAL\zhibinov <sergey.zhibinov@galaktika.ru>
2026-02-26 17:39:44 +0300

    Quick fix

--- Barcoding/Modules.Barcoding.MockInfrastructure/Database/DbConsts.cs
+++ Barcoding/Modules.Barcoding.MockInfrastructure/Database/DbConsts.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Modules.Barcoding.MockInfrastructure.Database
+namespace Modules.Barcoding.MockInfrastructure.Database;
+
+public static class DbConsts
 {
-	public static class DbConsts
-	{
-		public const string MockBarcodingSchemaName = "mock_barcoding";
-		public const string MigrationHistoryTableName = "migration_history";
-	}
+	public const string MockBarcodingSchemaName = "mock_barcoding";
+	public const string MigrationHistoryTableName = "migration_history";
 }
